e9d6e7306cad247f8ae6f4999340723307b3cda9
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-06-30 23:55:02 +0700

    Fixed for guest route in userInput.py

--- routes/userInput.py
+++ routes/userInput.py
@@ -67,7 +67,7 @@ async def submit_user_input(input_data: UserInputRequest, db: db_dependency, cur
 
         # perform the sentiment analysis process with input_data.text
         sentiment_result = await process_text_for_sentiment(
-            input_data.text, db, current_user.user_id
+            input_data.text, db, user_id
         )
         all_results.append(sentiment_result)
 
@@ -83,7 +83,7 @@ async def submit_user_input(input_data: UserInputRequest, db: db_dependency, cur
                     text_to_analyze = row_data[0]
 
                     # perform the sentiment analysis process with input_data.uploadedFiles's rowdata
-                    analysis_result = await process_text_for_sentiment(text_to_analyze, db, current_user.user_id)
+                    analysis_result = await process_text_for_sentiment(text_to_analyze, db, user_id)
                     all_results.append(analysis_result)
 
     elif not input_data.text and not input_data.uploadedFiles:
